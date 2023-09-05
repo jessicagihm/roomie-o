3d4ed68ecfaac203f89d15ba9ee28dba0768f93b
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-05 15:04:42 -0700

    signupformnav

--- ghi/src/RoomForm.js
+++ ghi/src/RoomForm.js
@@ -4,6 +4,7 @@ import useToken from "@galvanize-inc/jwtdown-for-react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import "./RoomForm.css";
 
+
 function RoomForm() {
   const [space, setSpace] = useState("");
   const [leaseType, setLeaseType] = useState("");
@@ -168,7 +169,7 @@ function RoomForm() {
       const responseData = await response.json();
       const roomId = responseData.room_id;
 
-      navigate(`/roomslist${roomId}`);
+      navigate(`/rooms${roomId}`);
     } catch (error) {
       console.error("Could not list room:", error);
     }

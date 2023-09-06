35acf878788ff19518d0d8c197848a4bb9792c85
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-06 16:09:33 -0700

    yestests

--- api/tests/test_get_rooms.py
+++ api/tests/test_get_rooms.py
@@ -1,21 +1,21 @@
-# from fastapi.testclient import TestClient
-# from main import app
-# from rooms.queries.rooms import RoomQueries
+from fastapi.testclient import TestClient
+from main import app
+from rooms.queries.rooms import RoomQueries
 
-# client = TestClient(app)
-
-
-# class EmptyRoomQueries:
-#     def get_all_rooms(self):
-#         return []
+client = TestClient(app)
 
 
-# def test_get_all_rooms():
-#     app.dependency_overrides[RoomQueries] = EmptyRoomQueries
+class EmptyRoomQueries:
+    def get_all_rooms(self):
+        return []
 
-#     response = client.get("/api/rooms")
 
-#     app.dependency_overrides = {}
+def test_get_all_rooms():
+    app.dependency_overrides[RoomQueries] = EmptyRoomQueries
 
-#     assert response.status_code == 200
-#     assert response.json() == {"rooms": []}
+    response = client.get("/api/rooms")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == {"rooms": []}

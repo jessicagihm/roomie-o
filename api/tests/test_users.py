6767c4dd6ca49bfc446d4174951a47d0295782b9
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-06 16:35:10 -0700

    justrooms

--- api/tests/test_users.py
+++ api/tests/test_users.py
@@ -1,30 +1,30 @@
-from fastapi.testclient import TestClient
-from main import app
-from users.queries.users import UserList
+# from fastapi.testclient import TestClient
+# from main import app
+# from users.queries.users import UserList
 
 
-client = TestClient(app)
+# client = TestClient(app)
 
 
-class EmptyUserQueries:
-    def get_users(self):
-        return []
+# class EmptyUserQueries:
+#     def get_users(self):
+#         return []
 
 
-def test_get_all_users():
+# def test_get_all_users():
 
-    app.dependency_overrides[UserList] = EmptyUserQueries
-    # ARRANGE
+#     app.dependency_overrides[UserList] = EmptyUserQueries
+#     # ARRANGE
 
-    # ACT
-    response = client.get("/api/users")
+#     # ACT
+#     response = client.get("/api/users")
 
-    app.dependency_overrides = {}
-    # ASSERT
+#     app.dependency_overrides = {}
+#     # ASSERT
 
-    assert response.status_code == 200
-    assert response.json() == {"users": []}
+#     assert response.status_code == 200
+#     assert response.json() == {"users": []}
 
 
-def test_init():
-    assert 1 == 1
+# def test_init():
+#     assert 1 == 1

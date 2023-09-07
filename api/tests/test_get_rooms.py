62785e444d0b6b7c6dcd4af1c0c91ce075cd6576
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-06 17:17:46 -0700

    tests

--- api/tests/test_get_rooms.py
+++ api/tests/test_get_rooms.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
 from main import app
 from rooms.queries.rooms import RoomQueries
-export DATABASE_URL = "postgresql://admin:secret@postgres/meme-team-supreme"
+
+import os
+os.environ["DATABASE_URL"] = "postgresql://admin:secret@postgres/meme-team-supreme"
 
 client = TestClient(app)
 
@@ -12,11 +14,10 @@ class EmptyRoomQueries:
 
 
 def test_get_all_rooms():
-    app.dependency_overrides[RoomQueries] = EmptyRoomQueries
+    app.dependency_overrides[RoomQueries] = EmptyRoomQueries()
 
     response = client.get("/api/rooms")
 
-    app.dependency_overrides = {}
+    app.dependency_overrides.pop(RoomQueries, None)
 
     assert response.status_code == 200
-    assert response.json() == {"rooms": []}

24eb662d40d4ae2d2f885f13de8a17512eaa916f
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-07 09:50:35 -0700

    allusertest

--- api/tests/test_users.py
+++ api/tests/test_users.py
@@ -1,30 +1,31 @@
-# from fastapi.testclient import TestClient
-# from main import app
-# from users.queries.users import UserList
+from fastapi.testclient import TestClient
+from authenticator import authenticator
+from main import app
+from users.queries.users import UserQueries
 
 
-# client = TestClient(app)
+client = TestClient(app)
 
 
-# class EmptyUserQueries:
-#     def get_users(self):
-#         return []
+class EmptyUserQueries:
+    def get_users(self):
+        return []
 
 
-# def test_get_all_users():
+def test_get_all_users():
 
-#     app.dependency_overrides[UserList] = EmptyUserQueries
-#     app.dependency_overrides[(authenticator.get_current_account)]
-#     # ARRANGE
+    app.dependency_overrides[UserQueries] = EmptyUserQueries
+    app.dependency_overrides[authenticator.try_get_current_account] = EmptyUserQueries
+    # ARRANGE
 
-#     # ACT
-#     response = client.get("/api/users")
+    # ACT
+    response = client.get("/api/users")
 
-#     app.dependency_overrides = {}
-#     # ASSERT
+    app.dependency_overrides = {}
+    # ASSERT
 
-#     assert response.status_code == 200
-#     assert response.json() == {"users": []}
+    assert response.status_code == 200
+    assert response.json() == {"users": []}
 
 
 def test_init():

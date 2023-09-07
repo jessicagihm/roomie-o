35ddc93a1f0fb074856485100a9af168263072cd
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-07 12:27:48 -0700

    forealz

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -22,11 +22,6 @@ function Nav() {
           <NavLink className="btn btn-light me-2" to="/users/">
             UsersList
           </NavLink>
-          {/* <NavLink className="btn btn-light me-2" to="/rooms/">
-            RoomsList
-          </NavLink> */}
-            User
-          </NavLink>
         </div>
       </div>
     </nav>

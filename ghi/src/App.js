f35c6ae606751e75d5a16f6a23259dcc1b7c6e1c
Leon Moun <moun.leon@gmail.com>
2023-09-08 02:45:06 +0000

    Built Landing/Home Page | Refactored Global CSS | Unit Test for Deleting a Room

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,14 +1,14 @@
-// import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
-import Nav from "./Nav";
-import "./App.css";
-import RoomDetails from "./RoomDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Nav from "./Nav";
+import LandingPage from "./LandingPage";
+import RoomDetails from "./RoomDetails";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
 import RoomForm from "./RoomForm";
-import MainPage from "./MainPage";
 import HomePage from "./HomePage";
 import PreferenceCreateForm from "./PreferencesForm";
 import UsersList from "./UsersList";
@@ -23,8 +23,8 @@ function App() {
           <Nav />
           <div className="container">
             <Routes>
-              <Route path="/" element={<MainPage />} />
               <Route path="/login" element={<LoginForm />} />
+              <Route path="/" element={<LandingPage />} />
               <Route path="/home" element={<HomePage />} />
               <Route path="/rooms/create" element={<RoomForm />} />
               <Route path="/rooms/:roomId" element={<RoomDetails />} />

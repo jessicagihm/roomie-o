3dd256cebee01e00c6fa81f7de21a29a33266ce3
Hudson Quitter <quitterh75@gmail.com>
2023-09-08 18:50:09 +0000

    frontend babayyyy

--- ghi/src/RoomsList.js
+++ ghi/src/RoomsList.js
@@ -1,84 +1,81 @@
-import React, { useEffect, useState } from "react";
-import useToken from "@galvanize-inc/jwtdown-for-react";
-import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import './RoomsList.css';
+// import React, { useEffect, useState } from "react";
+// import useToken from "@galvanize-inc/jwtdown-for-react";
+// import Card from "react-bootstrap/Card";
+// import { Link } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import './RoomsList.css';
 
+// function RoomsList() {
+//   const { token } = useToken();
+//   const [rooms, setRooms] = useState([]);
+//   const navigate = useNavigate();
 
-function RoomsList() {
-  const { token } = useToken();
-  const [rooms, setRooms] = useState([]);
-  const navigate = useNavigate();
+//   const fetchRooms = async () => {
+//     try {
+//       const response = await fetch("http://localhost:8000/api/rooms", {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       });
 
-  const fetchRooms = async () => {
-    try {
-      const response = await fetch("http://localhost:8000/api/rooms", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+//       if (!response.ok) {
+//         throw new Error(
+//           `Network response was not ok (status ${response.status})`
+//         );
+//       }
 
-      if (!response.ok) {
-        throw new Error(
-          `Network response was not ok (status ${response.status})`
-        );
-      }
+//       const data = await response.json();
+//       setRooms(data.rooms);
+//     } catch (error) {
+//       console.error("Error fetching room data:", error);
+//     }
+//   };
 
-      const data = await response.json();
-      setRooms(data.rooms);
-    } catch (error) {
-      console.error("Error fetching room data:", error);
-    }
-  };
+//   useEffect(() => {
+//     if (token) {
+//       fetchRooms();
+//     }
+//   }, [token]);
 
-  useEffect(() => {
-    if (token) {
-      fetchRooms();
-    }
-  }, [token]);
+//   function capFirstLetter(string) {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
 
+//   return (
+//      <div className="rooms-container">
+//       {rooms
+//       ? rooms.map((room) => {
+//           const [firstName, lastName] = typeof room.user_id === 'string' && room.user_id.includes(' ')
+//         ? room.user_id.split(' ')
+//         : ['', ''];
 
-  function capFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+//           const displayName = firstName && lastName ? `${firstName} ${lastName}` : room.user_id;
 
+//           return (
+//             <div key={room.id}>
+//               <button onClick={() => navigate("/")} className="unstyled-button">
+//                 <Card style={{ width: "18rem" }}>
+//                   <Card.Img variant="top" src={room.image} />
+//                   <Card.Body>
+//                     <Card.Title>
+//                       {displayName}
+//                     </Card.Title>
+//                     <Card.Text>
+//                       {capFirstLetter(room.space)}
+//                       <div>
+//                         {capFirstLetter(room.city)}, {room.state}
+//                       </div>
+//                     </Card.Text>
+//                     <Link to={`/rooms/${room.id}`}></Link>
+//                   </Card.Body>
+//                 </Card>
+//               </button>
+//             </div>
+//           );
+//         })
+//       : null}
+//     </div>
+//   );
+// }
 
-  return (
-     <div className="rooms-container">
-      {rooms
-      ? rooms.map((room) => {
-          const [firstName, lastName] = typeof room.user_id === 'string' && room.user_id.includes(' ')
-        ? room.user_id.split(' ')
-        : ['', ''];
-
-          const displayName = firstName && lastName ? `${firstName} ${lastName}` : room.user_id;
-
-          return (
-            <div key={room.id}>
-              <button onClick={() => navigate("/")} className="unstyled-button">
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img variant="top" src={room.image} />
-                  <Card.Body>
-                    <Card.Title>
-                      {displayName}
-                    </Card.Title>
-                    <Card.Text>
-                      {capFirstLetter(room.space)}
-                      <div>
-                        {capFirstLetter(room.city)}, {room.state}
-                      </div>
-                    </Card.Text>
-                    <Link to={`/rooms/${room.id}`}></Link>
-                  </Card.Body>
-                </Card>
-              </button>
-            </div>
-          );
-        })
-      : null}
-    </div>
-  );
-}
-
-export default RoomsList;
+// export default RoomsList;

ce32d52a17ff41c24df52b8d8b5b712b029fb4eb
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-07 19:18:39 -0700

    signup form | frontend deployment start

--- ghi/src/SignUpForm.js
+++ ghi/src/SignUpForm.js
@@ -16,6 +16,10 @@ function SignUpForm() {
   const { register } = useToken();
   const navigate = useNavigate();
 
+  const handlePicture = (e) => {
+    picture: picture ? URL.createObjectURL(picture.file) : null,
+  }
+
   const handleRegistration = (e) => {
     e.preventDefault();
     const accountData = {
@@ -109,7 +113,7 @@ function SignUpForm() {
             <label className="form-label">Image</label>
             <input
               name="image"
-              type="text"
+              type="file"
               className="form-control"
               onChange={(e) => {
                 setImage(e.target.value);

95bd783f68f9dc00d5dfdc45f234cb3d27899ffe
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-07 19:18:39 -0700

    signup form | frontend deployment start

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -23,24 +23,24 @@ function App() {
   const basename = process.env.PUBLIC_URL.replace(domain, "");
 
   return (
-    <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
-      <Router basename={basename}>
-        <div>
-          <Nav />
-          <div className="container">
-            <Routes>
-              <Route path="/login" element={<LoginForm />} />
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/rooms/create" element={<RoomForm />} />
-              <Route path="/rooms/:roomId" element={<RoomDetails />} />
-              <Route path="/rooms" element={<RoomsList />} />
-              <Route path="/signup" element={<SignUpForm />} />
-              <Route path="/preferences" element={<PreferenceCreateForm />} />
-              <Route path="/users" element={<UsersList />} />
-              <Route path="/preferences/:user_id" element={<PreferenceDisplay />}
-              />
-            </Routes>
+    <BrowserRouter basename={basename}>
+      <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
+        <Router>
+          <div>
+            <Nav />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/rooms/create" element={<RoomForm />} />
+                <Route path="/rooms/:roomId" element={<RoomDetails />} />
+                {/* <Route path="/rooms" element={<RoomsList />} /> */}
+                <Route path="/signup" element={<SignUpForm />} />
+                <Route path="/preferences" element={<PreferenceCreateForm />} />
+                <Route path="/users" element={<UsersList />} />
+              </Routes>
+            </div>
           </div>
         </Router>
       </AuthProvider>

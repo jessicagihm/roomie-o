dc5be696d756d089a0c8b8b64c9baa71af119403
Danielle Chang <dcha599@gmail.com>
2023-09-08 01:03:40 +0000

    Unit test for GET all rooms | Added protected endpoint

--- api/tests/test_get_rooms.py
+++ api/tests/test_get_rooms.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
 from rooms.queries.rooms import RoomQueries
+from authenticator import authenticator
 
 client = TestClient(app)
 
@@ -10,8 +11,26 @@ class EmptyRoomQueries:
         return []
 
 
+def fake_get_current_account_data():
+    return {
+        "picture": "string",
+        "space": "string",
+        "user_id": 1,
+        "created": "2023-09-07",
+        "city": "string",
+        "state": "ca",
+        "available_rooms": 0,
+        "cost": 0,
+        "lease_type": "string",
+        "description": "string",
+        "pets_allowed": True,
+        "bathrooms": 0
+    }
+
+
 def test_get_all_rooms():
     app.dependency_overrides[RoomQueries] = EmptyRoomQueries
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
 
     response = client.get("/api/rooms")
 
--- ghi/src/RoomsList.js
+++ ghi/src/RoomsList.js
@@ -1,52 +1,84 @@
-// import React, { useEffect, useState } from "react";
-// import Card from "react-bootstrap/Card";
-// import { Link } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import useToken from "@galvanize-inc/jwtdown-for-react";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import './RoomsList.css';
 
-// function RoomsList() {
-//   const [rooms, setRooms] = useState();
-//   const navigate = useNavigate();
 
-//   const getData = async () => {
-//     const response = await fetch("http://localhost:8000/api/rooms");
-//     const data = await response.json();
-//     setRooms(data.rooms);
-//   };
+function RoomsList() {
+  const { token } = useToken();
+  const [rooms, setRooms] = useState([]);
+  const navigate = useNavigate();
 
-//   useEffect(() => {
-//     getData();
-//   }, []);
+  const fetchRooms = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/api/rooms", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-//   return (
-//     <div>
-//       {rooms
-//         ? rooms.map((room) => {
-//             return (
-//               <div>
-//                 <button
-//                   onClick={() => navigate("/")}
-//                   className="unstyled-button"
-//                 >
-//                   <Card key={room.id} style={{ width: "18rem" }}>
-//                     <Card.Img variant="top" src={room.image} />
-//                     <Card.Body>
-//                       <Card.Title>
-//                         {room.user_id} {room.space}
-//                         {room.available_rooms} {room.bathrooms}
-//                         {room.city} {room.state}
-//                         {room.user_id} {room.space}
-//                       </Card.Title>
-//                       <Card.Text>About Me</Card.Text>
-//                       <Link to={`/rooms/${room.id}`}></Link>
-//                     </Card.Body>
-//                   </Card>
-//                 </button>
-//               </div>
-//             );
-//           })
-//         : null}
-//     </div>
-//   );
-// }
+      if (!response.ok) {
+        throw new Error(
+          `Network response was not ok (status ${response.status})`
+        );
+      }
 
-// export default RoomsList;
+      const data = await response.json();
+      setRooms(data.rooms);
+    } catch (error) {
+      console.error("Error fetching room data:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchRooms();
+    }
+  }, [token]);
+
+
+  function capFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+  return (
+     <div className="rooms-container">
+      {rooms
+      ? rooms.map((room) => {
+          const [firstName, lastName] = typeof room.user_id === 'string' && room.user_id.includes(' ')
+        ? room.user_id.split(' ')
+        : ['', ''];
+
+          const displayName = firstName && lastName ? `${firstName} ${lastName}` : room.user_id;
+
+          return (
+            <div key={room.id}>
+              <button onClick={() => navigate("/")} className="unstyled-button">
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={room.image} />
+                  <Card.Body>
+                    <Card.Title>
+                      {displayName}
+                    </Card.Title>
+                    <Card.Text>
+                      {capFirstLetter(room.space)}
+                      <div>
+                        {capFirstLetter(room.city)}, {room.state}
+                      </div>
+                    </Card.Text>
+                    <Link to={`/rooms/${room.id}`}></Link>
+                  </Card.Body>
+                </Card>
+              </button>
+            </div>
+          );
+        })
+      : null}
+    </div>
+  );
+}
+
+export default RoomsList;

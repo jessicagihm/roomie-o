8c5910847cbc3ca77b7b5948c281b18ff594fdda
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-07 17:40:32 -0700

    navlink

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -27,11 +27,6 @@ function Nav() {
               className="custom-logo-img"
             />
           </NavLink>
-          {/* <NavLink className="btn btn-light me-2" to="/rooms/">
-            RoomsList
-          </NavLink> */}
-            User
-          </NavLink>
         </div>
         <div
           className={`navbar-center ${

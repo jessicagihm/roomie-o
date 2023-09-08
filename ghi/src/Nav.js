9d3e9c04214b708a7965c718b5150cb17e3369df
Jessica Gihm <jessica,gihm@gmail.com>
2023-09-07 17:40:32 -0700

    navlink

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -36,11 +36,6 @@ function Nav() {
           <NavLink className="btn btn-light me-2" to="/users/">
             UsersList
           </NavLink>
-          {/* <NavLink className="btn btn-light me-2" to="/rooms/">
-            RoomsList
-          </NavLink> */}
-            User
-          </NavLink>
         </div>
       </div>
     </nav>
